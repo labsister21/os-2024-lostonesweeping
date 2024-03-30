75ef42089ea7920c1148351f7ff70c4dfa66ab15
InTheRain <indrswara@gmail.com>
2024-03-30 11:08:14 +0700

    add, shift, ctrl, capslock, alt

--- src/header/driver/keyboard.h
+++ src/header/driver/keyboard.h
@@ -11,8 +11,18 @@
 #define EXT_SCANCODE_LEFT      0x4B
 #define EXT_SCANCODE_RIGHT     0x4D
 
+//extended 
+#define EXT_SCANCODE_LEFT_SHIFT 0x36
+#define EXT_SCANCODE_RIGHT_SHIFT 0x2A 
+#define EXT_SCANCODE_ALT 0x38
+#define EXT_SCANCODE_CTRL 0x1D
+#define EXT_SCANCODE_CAPSLOCK 0x3A
+
 #define KEYBOARD_DATA_PORT     0x60
 #define EXTENDED_SCANCODE_BYTE 0xE0
+
+#define BUTTON_PRESSED 0x00
+#define BUTTON_RELEASED 0x80
 
 /**
  * keyboard_scancode_1_to_ascii_map[256], Convert scancode values that correspond to ASCII printables
@@ -21,7 +31,7 @@
  * By default, QEMU using scancode set 1 (from empirical testing)
  */
 extern const char keyboard_scancode_1_to_ascii_map[256];
-
+extern const char keyboard_scancode_1_to_ascii_map_shift_pressed[256];
 /**
  * KeyboardDriverState - Contain all driver states
  * 
@@ -34,6 +44,11 @@ struct KeyboardDriverState {
     bool read_extended_mode;
     bool keyboard_input_on;
     char keyboard_buffer;
+    bool shift_pressed; 
+    bool alt_pressed; 
+    bool ctrl_pressed; 
+    bool caps_active;
+    bool buffer_filled;
 } __attribute((packed));
 
 
