f7ae184bcd896fc9833caa0aa32a5db835582aff
Intermaze <rafikiprawhira@gmail.com>
2024-04-05 07:08:34 +0700

    feat: delete() file tested (folder?)

--- src/code/filesystem/fat32.c
+++ src/code/filesystem/fat32.c
@@ -409,7 +409,7 @@ int8_t write(struct FAT32DriverRequest request){
 
 int8_t delete(struct FAT32DriverRequest request){
     //dir_table merupakan directoryTable milik parent
-    struct FAT32DirectoryTable *dir_table;
+    struct FAT32DirectoryTable *dir_table = &fat32_driver_state.dir_table_buf;
     bool isParentValid = get_dir_table_from_cluster(request.parent_cluster_number, dir_table);
     if (!isParentValid){
         return -1;
@@ -421,8 +421,8 @@ int8_t delete(struct FAT32DriverRequest request){
     int rc; //request cluster
     for(rc = 0; rc<TOTAL_DIRECTORY_ENTRY; rc++){
         if(dir_table->table[rc].user_attribute == UATTR_NOT_EMPTY
-        && strcmp(dir_table->table[rc].name, request.name, 8)
-        && (strcmp(dir_table->table[rc].ext, request.ext, 3))
+        && strcmp(dir_table->table[rc].name, request.name, 8) == 0
+        && (strcmp(dir_table->table[rc].ext, request.ext, 3) == 0)
         ){
             found = true;
             isFolder = dir_table->table[rc].attribute == ATTR_SUBDIRECTORY; //Perlu testing, apakah jika request berupa folder ini true?
@@ -435,7 +435,7 @@ int8_t delete(struct FAT32DriverRequest request){
         if (isFolder){
             //Validasi bahwa folder kosong
             bool isKosong = true;
-            struct FAT32DirectoryTable *request_dt;
+            struct FAT32DirectoryTable *request_dt = {0};
             get_dir_table_from_cluster(rc, request_dt);
 
             int i; //i dimulai dari 2 karena 0 dan 1 berupa "." dan ".."
@@ -452,20 +452,36 @@ int8_t delete(struct FAT32DriverRequest request){
         }
 
         //Jalankan operasi delete
-        struct FAT32FileAllocationTable *fat_table = &fat32_driver_state.fat_table.cluster_map;
+        struct FAT32FileAllocationTable *fat_table = &fat32_driver_state.fat_table;
         int prev;
 
-        //Linked list allocationTable pada indeks rc jika ketemu file dalam folder
-        while (fat_table->cluster_map[rc] != FAT32_FAT_END_OF_FILE){
-            prev = rc;
-            rc = fat_table->cluster_map[rc];
-            fat_table->cluster_map[prev] = 0;
-        }
-        fat_table->cluster_map[rc] = 0;
+        //Kosongkan dir_table pada rc
+        // (Tidak semua dikosongkan, tetapi yang terpenting adalah .attribute)
+        dir_table->table[rc].attribute = !UATTR_NOT_EMPTY;
+        for (int i=0; i<8; i++){
+            dir_table->table[rc].name[i] = '\0';     
+        }  
+        for (int i=0; i<3; i++){
+            dir_table->table[rc].ext[i] = '\0';     
+        }  
 
-        //Delete dengan overwrite HDD dengan FAT dan dir_table yang dimodifikasi
+        //Linked list allocationTable pada indeks rc jika ketemu file dalam folder, sekaligus delete cluster isi file/folder
+        //rc++ Karena cluster indexing pada dir_table dimulai dari 0 (?)
+        rc++;
+        struct ClusterBuffer emptyBuffer = {0};
+        while (fat_table->cluster_map[rc] != FAT32_FAT_END_OF_FILE){
+            write_clusters(&emptyBuffer, rc, 1);
+            prev = rc;
+            rc = fat_table->cluster_map[rc]; 
+            fat_table->cluster_map[prev] = FAT32_FAT_EMPTY_ENTRY;
+        }
+        write_clusters(&emptyBuffer, rc, 1);
+        fat_table->cluster_map[rc] = FAT32_FAT_EMPTY_ENTRY;
+        
+        //Delete dengan overwrite HDD dengan FAT, dir_table, dan cluster yang dimodifikasi
         write_clusters(fat_table, FAT_CLUSTER_NUMBER, 1);
         write_clusters(dir_table, request.parent_cluster_number, 1);
+        
     }
 
     
