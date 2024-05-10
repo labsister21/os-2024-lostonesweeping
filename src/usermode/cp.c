e4577f350a22be053a826699c6451121685a8576
Indraswara <root@DESKTOP-4H8LC8L>
2024-05-10 14:03:07 +0700

    update

--- src/usermode/cp.c
+++ src/usermode/cp.c
@@ -5,4 +5,13 @@
 void cp(char* src, char* dest){
     uint32_t search_source_number = state.current_directory;
     uint32_t search_target_number = state.current_directory; 
+
+    char directories_src[10][12]; 
+    char directories_target[10][12]; 
+
+    int num_dir_src; 
+    int num_dir_target; 
+
+    extract_dir_special(src, num_dir_src, &num_dir_src); 
+    extract_dir_special(dest, num_dir_target, &num_dir_target);
 }
